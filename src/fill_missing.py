f2e0db9a0596fcda636abb1c637eeb1610a1c9fb
tphaterp <tphaterp@gmail.com>
2020-12-22 17:49:40 -0800

    changed how missing GarageYrBlt values were dealt with. Changed column type to int

--- src/fill_missing.py
+++ src/fill_missing.py
@@ -29,7 +29,7 @@ def main(train_path):
     # establishes what we want to fill the column NaNs with
     imput_dict = {"Alley":"no_access", "BsmtQual": "no_bsmt", "BsmtCond":"no_bsmt", 
                  "BsmtExposure":"no_bsmt", "BsmtFinType1":"no_bsmt", "BsmtFinType2":"no_bsmt",
-                 "FireplaceQu": "no_fireplace", "GarageType":"no_garage", "GarageYrBlt":"no_garage",
+                 "FireplaceQu": "no_fireplace", "GarageType":"no_garage",
                  "GarageFinish":"no_garage", "GarageQual":"no_garage", "GarageCond":"no_garage",
                  "PoolQC": "no_pool", "Fence": "no_fence", "MiscFeature": "none",'LotFrontage': 0,
                   'MasVnrType': "None", "MasVnrArea": 0 }
@@ -37,7 +37,17 @@ def main(train_path):
     # Make copy of original df and then apply missing data operations
 
     df.fillna(value = imput_dict, inplace = True)
+
+    #Impute year built for any missing GarageYrBuild value
+    df.loc[:,'GarageYrBlt'] = df['GarageYrBlt'].fillna(df.YearBuilt)
+    #df = df.astype({"GarageYrBlt": int})
+
+    #drops electrical row (1 observation)
     df.dropna(inplace = True)
+
+    # changes float to int
+    df = df.astype({"GarageYrBlt": int})
+
     df.to_csv("data/cleaned_train.csv", index = False)
 
 
